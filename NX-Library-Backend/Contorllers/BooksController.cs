4c23ea758a74c2cfba58190e87eb12da32a010e2
Jacob Smith <work.jacobsmith@gmail.com>
2025-02-14 09:17:20 -0700

    Book Controller added

---
+++ NX-Library-Backend/Contorllers/BooksController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NX_Library_Backend.Data;
+using Models;
+using NXLibraryBackend.Data;
+
+namespace NXLibraryBackend.BookController
+{
+    [ApiController]
+    [Route("[controller]"), Authorize]
+    public class BookContorller(NXLibDbContext _ctx) : Controller
+    {
+        [HttpGet("GetBooks")]
+        public async Task<List<Book>> GetBooks()
+        {
+
+            var rslt = from c in _ctx.Books
+                       select c;
+            return await rslt.ToListAsync();
+
+        }
+
+        [HttpGet("GetBook/{bookId}")]
+        public async Task<Book?> GetBook(int bookId)
+        {
+            var rslt = from c in _ctx.Books
+                       where c.Id == bookId
+                       select c;
+            return await rslt.FirstOrDefaultAsync();
+        }
+
+        [HttpPost("AddBook")]
+        public async Task<ActionResult> AddBook(Book book)
+        {
+            _ctx.Books.Add(book);
+            await _ctx.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("/UpdateBook")]
+        public async Task<ActionResult> UpdateBook(Book book)
+        {
+            _ctx.Books.Update(book);
+            await _ctx.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("/DeleteBook/{bookId}")]
+        public async Task<ActionResult> DeleteBook(int bookId)
+        {
+            var rslt = from c in _ctx.Books
+                       where c.Id == bookId
+                       select c;
+            Book? book = await rslt.FirstOrDefaultAsync();
+            if (book != null)
+            {
+                _ctx.Books.Remove(book);
+                await _ctx.SaveChangesAsync();
+            }
+            return Ok();
+        }
+    }
+}
